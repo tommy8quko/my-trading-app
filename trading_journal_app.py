48d35007147ee20d69a94f3e51c6e1834956af7a
tommy8quko <huitszkit@gmail.com>
2026-01-26 15:35:35 +0800

    Update trading_journal_app.py

--- trading_journal_app.py
+++ trading_journal_app.py
@@ -739,8 +739,8 @@ with t1:
 
 with t2:
     st.markdown("### 🟢 持倉概覽")
-    
-     if 'edit_mode' not in st.session_state:
+
+    if 'edit_mode' not in st.session_state:
         st.session_state.edit_mode = False
         st.session_state.edit_symbol = None
          
@@ -946,6 +946,7 @@ with t5:
         save_all_data(pd.DataFrame(columns=df.columns))
         st.success("數據已清空")
         st.rerun()
+
 
 
 
